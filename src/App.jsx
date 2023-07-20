4e1c2992775ac5c60c308a5703876701521bf2a0
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2023-07-19 21:04:15 -0700

    Code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 function App() {
   return (
     <div>
-      <header>
-        <p>Edit and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
