54dbef45353730d1c506ae496e6daf7c9bbd7dae
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2023-07-21 14:27:11 -0700

    Added routes and loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Layout from "./components/Layout";
+import Loader from "./components/UI/Loader";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+  const router = (
+    <Router basename="/magic-flowers-boutique">
+      <React.Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </React.Suspense>
+    </Router>
   );
+
+  return router;
 }
 
 export default App;
