b7662ce9051e68e3ae213b342e0667e64a54b0b4
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2023-07-21 19:04:59 -0700

    Added basic variables, media screens, and mixins. Code refactoring

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
+import classes from "./Layout.module.scss";
+// TODO: Fix modules declaration
 
 const Layout: React.FC = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className={classes.main}>
+        {/* <main> */}
         <Outlet />
       </main>
       <Footer />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react';
-import sass from 'vite-plugin-sass';
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   base: "/magic-flowers-boutique/",
-  plugins: [react(), sass()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
