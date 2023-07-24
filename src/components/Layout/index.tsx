a03f8a5c02f013604b25113602f5557b8bb081ec
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2023-07-24 12:24:35 -0700

    Fixed type declaration and changed favicon / logo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,14 +3,12 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 import classes from "./Layout.module.scss";
-// TODO: Fix modules declaration
 
 const Layout: React.FC = () => {
   return (
     <>
       <Header />
       <main className={classes.main}>
-        {/* <main> */}
         <Outlet />
       </main>
       <Footer />
