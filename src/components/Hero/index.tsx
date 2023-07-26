b48e2bd3e88bbde6d679507f80f680cfc92d7f4f
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2023-07-25 19:52:02 -0700

    Created section Hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Section from "../Section";
+import Button from "../UI/Button";
+import classes from "./Hero.module.scss";
+
+const Hero: React.FC = () => {
+  return (
+    <Section className={classes.hero}>
+      <p className={classes.hero__text}>Welcome to Magic Flowers Boutique</p>
+      <h1 className={classes.hero__title_main}>
+        Transforming <br /> Moments into <br /> Memories
+      </h1>
+
+      <h2 className={classes.hero__title}>
+        Surprise your loved ones today
+        <br /> with our fast and exquisite floral creations!
+      </h2>
+
+      <Button className={classes.hero__button} type="button" onClick={() => {}}>
+        Explore our services
+      </Button>
+    </Section>
+  );
+};
+
+export default Hero;
