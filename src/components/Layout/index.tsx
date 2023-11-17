c78e6d0e08c918d21270d824dc5b61c752aec42d
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2023-11-17 15:46:52 -0800

    Create responsive layout style

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { FunctionComponent } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 import classes from "./Layout.module.scss";
 
-const Layout: React.FC = () => {
+const Layout: FunctionComponent = () => {
   return (
     <>
       <Header />
-      <main className={classes.main}>
+      <main className={classes.Layout__main}>
         <Outlet />
       </main>
       <Footer />
--- src/components/UI/Section/index.tsx
+++ src/components/UI/Section/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { LayoutProps } from "../../../types";
+import React, { FunctionComponent } from "react";
+import { LayoutProps } from "../../Layout/Layout.types";
 
-const Section: React.FC<LayoutProps> = ({ children, className }) => {
+const Section: FunctionComponent<LayoutProps> = ({ children, className }) => {
   const sectionClasses = className || "";
 
   return <section className={sectionClasses}>{children}</section>;
