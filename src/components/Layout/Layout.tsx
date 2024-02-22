b05f9bcdfc16942ce967abea018c089e1adf21a5
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2024-02-21 20:10:14 -0800

    Add scroll button

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import ScrollButton from "../ScrollButton/ScrollButton";
 import classes from "./Layout.module.scss";
 
 const Layout: FunctionComponent = () => {
@@ -10,6 +11,7 @@ const Layout: FunctionComponent = () => {
       <main className={classes.Layout__main}>
         <Header />
         <Outlet />
+        <ScrollButton />
       </main>
       <Footer />
     </>
