4aebb8ad9ef7c92b9353d0b950d95e0a9abff00c
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2024-02-25 18:27:42 -0800

    Add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppRouter from "./router/AppRouter";
 
-import HomePage from "./pages/HomePage/HomePage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import Layout from "./components/Layout/Layout";
-import Loader from "./components/UI/Loader/Loader";
-
-function App() {
-  const router = (
-    <Router basename="/magic-flowers-boutique">
-      <React.Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </React.Suspense>
-    </Router>
-  );
-
-  return router;
-}
+const App = () => {
+  return <AppRouter />;
+};
 
 export default App;
